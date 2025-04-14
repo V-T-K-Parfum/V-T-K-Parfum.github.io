3782de7b90f1752780ae82a50f43ae898592da87
Infinite <jotarokujo0539@gmail.com>
2025-04-14 12:23:52 -0400

    Initial commit

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('E-mail Vihaan,Tej, or Keshav! ' + document.getElementById('name').value + '!');
+    alert('E-mail Vihaan,Tej, or Keshav, ' + document.getElementById('name').value + '!');
 });
