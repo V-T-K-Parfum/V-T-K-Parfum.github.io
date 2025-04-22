370c152965ce8242b8b63dbfe8b9b6a591f90f3c
Infinite <jotarokujo0539@gmail.com>
2025-04-22 14:40:25 -0400

    Initial Commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    const subject = encodeURIComponent("New Contact from " + name);
+    const subject = encodeURIComponent("V.T.K Parfum Buyer" + name);
     const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\nMessage: [Type your message here]`);
 
     window.location.href = `mailto:kum0503@dcds.edu,var0902@dcds.edu,kar145070@dcds.edu?subject=${subject}&body=${body}`;
